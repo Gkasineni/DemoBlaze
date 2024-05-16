d567d5d5e427b2df99726e9adcffe66b1da1c221
Gkasineni <gopikasineni1996@gmail.com>
2024-05-16 18:52:54 +1000

    first commit

---
+++ cypress/Integration/examples/browsercontrols.js
@@ -0,0 +1,41 @@
+//browser controls and navigation
+
+
+describe('Browser contorls and navigation scenarios', ()=> {
+    it('Browser back button test',()=> {
+        cy.visit("https://www.demoblaze.com/index.html")
+        cy.wait(2000)
+        cy.get('#cartur').click()
+        cy.url().should('include','https://www.demoblaze.com/cart.html')
+        cy.wait(2000)
+        cy.go('back') //browser back button should be selected
+        cy.url().should('include','https://www.demoblaze.com/index.html')
+    })
+
+    it('Browser forward button test',()=> {
+        cy.visit("https://www.demoblaze.com/index.html")
+        cy.wait(2000)
+        cy.get('#cartur').click()
+        cy.url().should('include','https://www.demoblaze.com/cart.html')
+        cy.wait(2000)
+        cy.go('back') //browser back button should be selected
+        cy.wait(2000)
+        cy.url().should('include','https://www.demoblaze.com/index.html')
+        cy.wait(2000)
+        cy.go('forward')
+        cy.url().should('include','https://www.demoblaze.com/cart.html')
+        cy.wait(2000)
+
+    })
+
+    it('Browser navigation to footer and header section os a page', ()=>{
+        cy.visit('https://www.demoblaze.com/index.html')
+        cy.scrollTo('bottom')
+        cy.wait(2000)
+        cy.get("p[class='m-0 text-center text-white']").should('have.text', 'Copyright © Product Store 2017') //footer element
+        cy.wait(2000)
+        cy.scrollTo('top')
+        cy.wait(2000)
+        cy.get('a[id="nava"]').should('be.visible') //header element 
+    })
+})
